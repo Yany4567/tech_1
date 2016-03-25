83ef89f6c2de3423cdfb0f4b83b9ae9bb82c7813
lanou3g <lanou3g@lanou3gdeMac-mini-9.local>
2016-03-25 14:22:14 +0800

    asdddddd

---
+++ lianxi/lianxi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  lianxi
+//
+//  Created by lanou3g on 16/3/25.
+//  Copyright © 2016年 高艳闯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ lianxi/lianxi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  lianxi
+//
+//  Created by lanou3g on 16/3/25.
+//  Copyright © 2016年 高艳闯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
